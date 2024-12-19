bdd26b63b0cd153589f242b2f058f96200578a6a
tanzila009 <tanzilakhusanovaa@gmail.com>
2024-12-19 17:00:06 +0500

    pdp

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello world")
+
+"""
+echo "# pdp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/tanzila009/pdp.git
+git push -u origin main
+"""
